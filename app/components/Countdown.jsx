c61add1dacebaac2efc270959fb7a6af313cd954
Eric Hodges <ehodges2004@gmail.com>
2016-05-24 13:54:46 -0700

    Update status to stop when count reaches zero

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -10,7 +10,6 @@ var Countdown = React.createClass( {
       countdownStatus: 'stopped'
     };
   },
-
   componentDidUpdate: function(prevProps, prevState) {
     if(this.state.countdownStatus !== prevState.countdownStatus) {
       switch(this.state.countdownStatus) {
@@ -26,12 +25,20 @@ var Countdown = React.createClass( {
       }
     }
   },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
   startTimer: function() {
     this.timer = setInterval(() => {
       var newCount = this.state.count -1;
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+
+      if(newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   },
 
@@ -48,9 +55,9 @@ var Countdown = React.createClass( {
     var {count, countdownStatus} = this.state;
     var renderControlArea = () => {
       if(countdownStatus !== 'stopped') {
-        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
       } else {
-        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+        return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
       }
     };
 
